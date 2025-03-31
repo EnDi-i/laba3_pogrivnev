f631ac9a0a9d6646fd182d8e850019ecfa147568
Илья Залата <illiazalata@gmail.com>
2025-04-01 00:38:21 +0300

    виконано перше завдання

--- laba3_pogrivnev/2and13.cs
+++ laba3_pogrivnev/2and13.cs
@@ -1,12 +1,53 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using array;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace laba3_pogrivnev
+namespace zalata
 {
-    internal class _2and13
+    public class variant2and13
     {
+        public void Variant2(int[] myArray)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Варіант 2 - Знищити останній від’ємний елемент.");
+            ArrayMy arrayHelper = new ArrayMy();
+            myArray = DestroyLastElement(myArray);
+            arrayHelper.PrintArray(myArray);
+        }
+
+        static int FindLastElement(int[] arr)
+        {
+            int lastMinus = 0;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (arr[i] < 0)
+                {
+                    lastMinus = i;
+                }
+                else
+                {
+                    Console.WriteLine("Введений масив не має від'ємних чисел");
+                }
+            }
+            return lastMinus;
+        }
+
+        static int[] DestroyLastElement(int[] arr)
+        {
+            int lastMinus = FindLastElement(arr);
+            int[] newArr = new int[arr.Length - 1];
+            int index = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i != lastMinus)
+                {
+                    newArr[index++] = arr[i];
+                }
+            }
+            return newArr;
+        }
+
+
     }
 }
