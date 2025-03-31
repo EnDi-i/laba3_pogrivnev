1b5b7e2a177d787e10e46fc9e9d8731d1ae4743b
miroslavkononenko5@gmail.com <miroslavkononenko5@gmail.com>
2025-03-31 21:36:36 +0300

    Додано клас Varianti16_6 з методом Variant16
    У файл `16and6.cs` додано:
    - Пространства імен: `System`, `System.Text`, `array`, `static System.Runtime.InteropServices.JavaScript.JSType`.
    - Пространство імені `кононенко`.
    - Класс `Varianti16_6` всередині простору імен `kononenko`.
    - Метод `Variant16` в класі `Varianti16_6`, створюючий масив цілих чисел.

---
+++ laba3_pogrivnev/16and6.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using array;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace kononenko
+{
+    public class Varianti16_6
+    {
+        public void Variant16(int[] myArray)
+        {
+
+        }
+    }
+}
