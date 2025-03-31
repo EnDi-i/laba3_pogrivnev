fb9cfea30b450f4b0149dad3e3c998a92f5024ec
joustik <telega.3raza@gmail.com>
2025-03-31 21:41:44 +0300

    Додано клас Varianti16_6 з методом Variant16
    У файл 9and12.cs додано:
    Пространства імен: System, System.Text, array, static System.Runtime.InteropServices.JavaScript.JSType.
    Пространство імені сидоренко.
    Класс Varianti9_12 всередині простору імені sydorenko.
    Метод Variant9 в класі Varianti9_12, створюючий масив цілих чисел.

---
+++ laba3_pogrivnev/9and12.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba3_pogrivnev
+{
+    public class Variant9_12
+    {
+        public void Variant9(in[] myArray)
+        {
+
+        }
+    }
+}
